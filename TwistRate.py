584b0b4732e23a444e47330e867d376d3e1d5962
Viacheslav Bocharov <darkhaisam@gmail.com>
2015-11-09 17:51:03 +0300

    Добавлен параметр -g

--- TwistRate.py
+++ TwistRate.py
@@ -11,13 +11,16 @@ UtilName = 'Balistic Tools : Optimal Rifling Twist Rate '
 __author__ = 'DarkHaisam'
 __version__='0.1'
 
-print UtilName +__version__
+print UtilName +' ver '+__version__
 parser=argparse.ArgumentParser(description=UtilName + __version__)
 parser.add_argument('-v','--version', action='version', version='Balistic Tools ' '%(prog)s ' +'version '+ __version__)
 parser.add_argument('-V','--velocity', type=int, required=True, help='Velocity bullet (ft/s)')
 parser.add_argument('-D','--dbullet', type=float, required=True, help='Diametr  bullet (in)')
 parser.add_argument('-L','--lbullet', type=float, help='Length bullet (in)')
 parser.add_argument('-T','--twist', type=float, help='Twist range')
+parser.add_argument('-g','--greenhill', action='store_true', help='Use formula of Greenhill ')
+
+
 args = parser.parse_args()
 
 if args.velocity <= 0:
@@ -37,11 +40,15 @@ def m_lenght_bullet():
 	v_lbullets=round(((C*pow(args.dbullet,2))/args.twist),2)
 	return(v_lbullets)
 
-if args.lbullet is None and args.twist is None:
-	print "Option -T twist range or -L lenght bullet must by used"
-	exit()
-elif args.lbullet is not None:
-	print m_twist_rate()
-elif args.twist is not None:
-	print m_lenght_bullet()
+def formula_greenhill():
 
+	if args.lbullet is None and args.twist is None:
+		print "Option -T twist range or -L lenght bullet must by used"
+		exit()
+	elif args.lbullet is not None:
+		print 'Optimal Rifling Twist Rate '+  str(m_twist_rate()) + ' in.'
+	elif args.twist is not None:
+		print  'Optimal Rifling Twist Rate '+ srt(m_lenght_bullet()) + ' in.'
+
+if args.greenhill is True:
+	formula_greenhill()
