027a0d6c3b729e24992055f2734d47341e320bc5
Viacheslav Bocharov <darkhaisam@gmail.com>
2015-11-20 16:48:57 +0300

    Изменена структура модулей, Полноценный расчет по Формулек Гринхилла и Съерра Буллет

--- rpm_bullet.py
+++ rpm_bullet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Расчет начальной угловой скорости пули по шагу нарезов и дульной скорости скорости
+# Быстрый расчет начальной угловой скорости пули по шагу нарезов и дульной скорости скорости
 #
 #
 #
@@ -17,11 +17,13 @@ parser = argparse.ArgumentParser(description=UtilName + __version__)
 parser.add_argument('-v', '--version', action='version',
                     version='Balistic Tools ' '%(prog)s ' + 'version ' + __version__)
 parser.add_argument('-V', '--velocity', type=int, required=True, help='Velocity bullet (ft/s)')
-parser.add_argument('-T', '--twist', type=float, help='Twist range in in.')
-
+parser.add_argument('-T', '--twist', type=float, required=True, help='Twist range in in.')
 args = parser.parse_args()
 
 
 def rpm_bullet(twist, velocity):
     rpm = round(velocity / (twist * 0.0254))
     return (rpm)
+
+
+print 'RPM is - ' + str(rpm_bullet(args.twist, args.velocity))
